fa7bb762f38e41f98c2b01ffc8721b8cb8dbf272
Minh Huy <minhhuy211@gmail.com>
2018-05-30 19:34:52 +0700

    Test Toast

--- app/src/main/java/com/example/admin/testgithub/MainActivity.java
+++ app/src/main/java/com/example/admin/testgithub/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.admin.testgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Toast.makeText(this, "Hom Nay Di Hoc Thu 4", Toast.LENGTH_SHORT).show();
 
     }
 }
